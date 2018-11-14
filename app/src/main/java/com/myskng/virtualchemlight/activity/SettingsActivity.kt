bbbc038018e512f753d2a1944f71a1e005dcb408
Kazuki Oishi <oishikazuki@gmail.com>
2018-11-15 01:19:29 +0900

    Unused importを消した

--- app/src/main/java/com/myskng/virtualchemlight/activity/SettingsActivity.kt
+++ app/src/main/java/com/myskng/virtualchemlight/activity/SettingsActivity.kt
@@ -8,7 +8,6 @@ import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
 import androidx.preference.EditTextPreference
 import androidx.preference.PreferenceFragmentCompat
-import com.google.android.gms.oss.licenses.OssLicensesActivity
 import com.google.android.gms.oss.licenses.OssLicensesMenuActivity
 import com.myskng.virtualchemlight.R
 
